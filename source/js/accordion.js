78ba157f831494d29ff5325a2121eba0e0775e50
Alina Anisimova <anisimova.alina.leonidovna@gmail.ru>
2021-03-02 11:09:51 +0300

    Добавляет модальное окно

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -1,35 +1,37 @@
 "use strict";
 
-const accordions = document.querySelectorAll(`.page-footer__accordion`);
+(() => {
+  const accordions = document.querySelectorAll(`.page-footer__accordion`);
 
-const switchs = function (block) {
-  const btnSwitch = block.querySelector(`.page-footer__switch`);
+  const switchs = function (block) {
+    const btnSwitch = block.querySelector(`.page-footer__switch`);
 
-  const closeList = function (evt) {
-    if (!(block.classList.contains(`page-footer__accordion--close`))) {
-      evt.stopPropagation();
+    const closeList = (evt) => {
+      if (!(block.classList.contains(`page-footer__accordion--close`))) {
+        evt.stopPropagation();
 
-      block.classList.add(`page-footer__accordion--close`);
+        block.classList.add(`page-footer__accordion--close`);
 
-      btnSwitch.removeEventListener(`click`, closeList);
-    }
+        btnSwitch.removeEventListener(`click`, closeList);
+      }
+    };
+
+    block.addEventListener(`click`, () => {
+      for (let i = 0; i < accordions.length; i++) {
+        accordions[i].classList.add(`page-footer__accordion--close`);
+      }
+
+      block.classList.remove(`page-footer__accordion--close`);
+
+      btnSwitch.addEventListener(`click`, closeList);
+    });
   };
 
-  block.addEventListener(`click`, function () {
+  if (accordions) {
     for (let i = 0; i < accordions.length; i++) {
-      accordions[i].classList.add(`page-footer__accordion--close`);
+      accordions[i].classList.remove(`page-footer__accordion--no-js`);
+
+      switchs(accordions[i]);
     }
-
-    block.classList.remove(`page-footer__accordion--close`);
-
-    btnSwitch.addEventListener(`click`, closeList);
-  });
-};
-
-if (accordions) {
-  for (let i = 0; i < accordions.length; i++) {
-    accordions[i].classList.remove(`page-footer__accordion--no-js`);
-
-    switchs(accordions[i]);
   }
-}
+})();
--- source/js/modal.js
+++ source/js/modal.js
@@ -1,44 +1,6 @@
 "use strict";
 
 (() => {
-  const accordions = document.querySelectorAll(`.page-footer__accordion`);
-
-  const switchs = function (block) {
-    const btnSwitch = block.querySelector(`.page-footer__switch`);
-
-    const closeList = (evt) => {
-      if (!(block.classList.contains(`page-footer__accordion--close`))) {
-        evt.stopPropagation();
-
-        block.classList.add(`page-footer__accordion--close`);
-
-        btnSwitch.removeEventListener(`click`, closeList);
-      }
-    };
-
-    block.addEventListener(`click`, () => {
-      for (let i = 0; i < accordions.length; i++) {
-        accordions[i].classList.add(`page-footer__accordion--close`);
-      }
-
-      block.classList.remove(`page-footer__accordion--close`);
-
-      btnSwitch.addEventListener(`click`, closeList);
-    });
-  };
-
-  if (accordions) {
-    for (let i = 0; i < accordions.length; i++) {
-      accordions[i].classList.remove(`page-footer__accordion--no-js`);
-
-      switchs(accordions[i]);
-    }
-  }
-})();
-
-"use strict";
-
-(() => {
   const body = document.querySelector(`.page`);
   const linkModal = document.querySelector(`#modal`);
   const modal = document.querySelector(`.modal`);
